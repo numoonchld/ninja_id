a0c15449eb5f1d122e650cd288c1757dd8abc3c7
numoonchld <raghavendra.saralaya@gmail.com>
2021-01-04 18:28:11 +0530

    zeroth commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
